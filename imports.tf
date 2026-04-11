7cb424fcf989f2addbac9c72c0e1d275fc39aa32
CorpLynx <brichard3132@gmail.com>
2026-04-11 12:37:34 -0400

    Updating import and disabling the SA policy instead of deleting constraing

--- imports.tf
+++ imports.tf
@@ -30,7 +30,6 @@ import {
   to = module.organization-iam[0].google_org_policy_custom_constraint.constraint["custom.storageRequireBucketObjectVersionningV2"]
 }
 
-# TEMPORARILY COMMENTED OUT — matches commented-out constraint in custom-constraints yaml
 import {
   id = "organizations/1041701195417/customConstraints/custom.iamDisableAdminServiceAccountV2"
   to = module.organization-iam[0].google_org_policy_custom_constraint.constraint["custom.iamDisableAdminServiceAccountV2"]
